6546b6397b2b041a55e2cd9d157b28c69ec00da9
tonyg <aj.gianno@gmail.com>
2021-05-20 18:28:26 -0500

    redux init and boilerplate structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
+
+import App from './components/App/App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
