f88e49b890951a67efea50bdfafb00e80955c63f
tonyg <aj.gianno@gmail.com>
2021-05-20 18:38:54 -0500

    root reducer

---
+++ src/redux/reducers/messageReducer.js
@@ -0,0 +1,15 @@
+const messageReducer = () => {
+  return [
+    'Aww yes!',
+    'Nice job!',
+    'Hooray for you!',
+    'Sweet!',
+    'Yasssssss!',
+    "You're a star!",
+    'Way to go!',
+  ];
+};
+
+export default messageReducer;
+
+// export const getMessage = () => messages[(Math.random() * messages.length) | 0];
